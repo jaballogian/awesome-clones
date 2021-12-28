3446f2099a2cbb08a1a50cd791037d94b980a602
jaballogian <jaballogianrma77@gmail.com>
2021-12-28 19:29:26 +0700

    add colors

---
+++ src/pages/CssWaterDropLogoEffect/colors.js
@@ -0,0 +1,9 @@
+export const colors = {
+  gallery: '#EEEEEE', // GRAY
+  tundora: '#444444', // DARK GRAY
+}
+
+export const boxShadows = {
+  softBlack: 'rgba(0, 0, 0, 0.05)',
+  softLight: 'rgba(255, 255, 255, 0.05)',
+}
