06658a640c2617e564d369cc3eddaf6448113493
jaballogian <jaballogianrma77@gmail.com>
2021-12-28 17:40:34 +0700

    add Css Water Drop Logo Effect page and its styles

---
+++ src/pages/CssWaterDropLogoEffect/Index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+// STYELS
+import useStyles from './IndexUseStyles'
+
+const Index = () => {
+  const classes = useStyles()
+
+  return (
+    <div>
+      Css Water Drop Logo Effect
+    </div>
+  )
+}
+
+export default Index
