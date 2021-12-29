c2e0a6b6ecebf387a10c8c98e150ecdcdb2ec970
jaballogian <jaballogianrma77@gmail.com>
2021-12-29 20:47:53 +0700

    add colors and box shadows

--- src/pages/GlassmorphismCardHover/constants.js
+++ src/pages/GlassmorphismCardHover/constants.js
@@ -12,3 +12,16 @@ let customTheme = createTheme({
 customTheme = responsiveFontSizes(customTheme)
 
 export default customTheme
+
+export const colors = {
+  amaranth: '#E91E63', // PINKISH RED
+  blueRibbon: '#2169F3', // BLUE
+  cinder: '#161623', // DARK BLUE,
+}
+
+export const boxShadows = {
+  hardBlack: 'rgba(0, 0, 0, 0.5)',
+  hardWhite: 'rgba(255, 255, 255, 0.5)',
+  softBlack: 'rgba(0, 0, 0, 0.1)',
+  softWhite: 'rgba(255, 255, 255, 0.05)',
+}
