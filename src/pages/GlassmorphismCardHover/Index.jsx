b5a41f28fb8802500e14de9168a3dd1bfd7630b5
jaballogian <jaballogianrma77@gmail.com>
2021-12-29 20:00:39 +0700

    add Theme Provider and Styled Engine Provider components

--- src/pages/GlassmorphismCardHover/Index.jsx
+++ src/pages/GlassmorphismCardHover/Index.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 
+// CONSTANTS
+import customTheme from './constants'
+
 // MUIS
 import Box from '@mui/material/Box'
 import Card from '@mui/material/Card'
 import Link from '@mui/material/Link'
 import Typography from '@mui/material/Typography'
+
+// MUI STYLES
+import { StyledEngineProvider, ThemeProvider } from '@mui/material/styles'
 
 // STYLES
 import useStyles from './IndexUseStyles'
@@ -19,46 +25,50 @@ const Index = () => {
   ]
 
   return (
-    <Box className={classes.root}>
-      {titleList.map((item, index) => (
-        <Card 
-          key={index}
-          className={classes.card}
-        >
-          <Box className={classes.content}>
-            {/* NUMBER */}
-            <Typography
-              variant='h2'
-              className={classes.number}
+    <ThemeProvider theme={customTheme}>
+      <StyledEngineProvider injectFirst>
+        <Box className={classes.root}>
+          {titleList.map((item, index) => (
+            <Card 
+              key={index}
+              className={classes.card}
             >
-              {`0${index}`}
-            </Typography>
+              <Box className={classes.content}>
+                {/* NUMBER */}
+                <Typography
+                  variant='h2'
+                  className={classes.number}
+                >
+                  {`0${index}`}
+                </Typography>
 
-            {/* TITLE */}
-            <Typography
-              variant='h3'
-              className={classes.title}
-            >
-              {item}
-            </Typography>
+                {/* TITLE */}
+                <Typography
+                  variant='h3'
+                  className={classes.title}
+                >
+                  {item}
+                </Typography>
 
-            {/* PARAGRAPH */}
-            <Typography
-              variant='subtitle1'
-              className={classes.paragraph}
-            >
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-              sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            </Typography>
+                {/* PARAGRAPH */}
+                <Typography
+                  variant='subtitle1'
+                  className={classes.paragraph}
+                >
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                  sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                </Typography>
 
-            {/* LINK */}
-            <Link href='#'>
-              Read More
-            </Link>
-          </Box>
-        </Card>      
-      ))}
-    </Box>
+                {/* LINK */}
+                <Link href='#'>
+                  Read More
+                </Link>
+              </Box>
+            </Card>      
+          ))}
+        </Box>
+      </StyledEngineProvider>
+    </ThemeProvider>
   )
 }
 
