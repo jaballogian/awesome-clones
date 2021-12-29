5523b6331198f0cde9608f81afc9fc7b203eef70
jaballogian <jaballogianrma77@gmail.com>
2021-12-29 19:36:27 +0700

    add some components

--- src/pages/GlassmorphismCardHover/Index.jsx
+++ src/pages/GlassmorphismCardHover/Index.jsx
@@ -1,15 +1,64 @@
 import React from 'react'
 
+// MUIS
+import Box from '@mui/material/Box'
+import Card from '@mui/material/Card'
+import Link from '@mui/material/Link'
+import Typography from '@mui/material/Typography'
+
 // STYLES
 import useStyles from './IndexUseStyles'
 
 const Index = () => {
   const classes = useStyles()
 
+  const titleList = [
+    'Card One',
+    'Card One',
+    'Card One',
+  ]
+
   return (
-    <div>
-      Glassmorphism Card Hover
-    </div>
+    <Box className={classes.root}>
+      {titleList.map((item, index) => (
+        <Card 
+          key={index}
+          className={classes.card}
+        >
+          <Box className={classes.content}>
+            {/* NUMBER */}
+            <Typography
+              variant='h2'
+              className={classes.number}
+            >
+              {`0${index}`}
+            </Typography>
+
+            {/* TITLE */}
+            <Typography
+              variant='h3'
+              className={classes.title}
+            >
+              {item}
+            </Typography>
+
+            {/* PARAGRAPH */}
+            <Typography
+              variant='subtitle1'
+              className={classes.paragraph}
+            >
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+              sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </Typography>
+
+            {/* LINK */}
+            <Link href='#'>
+              Read More
+            </Link>
+          </Box>
+        </Card>      
+      ))}
+    </Box>
   )
 }
 
