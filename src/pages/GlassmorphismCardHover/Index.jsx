b048f5b9a08f25b1cec92e5c1fc76f0c231894b7
jaballogian <jaballogianrma77@gmail.com>
2021-12-29 19:24:25 +0700

    add Glassmorphism Card Hover page and its style file

---
+++ src/pages/GlassmorphismCardHover/Index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+// STYLES
+import useStyles from './IndexUseStyles'
+
+const Index = () => {
+  const classes = useStyles()
+
+  return (
+    <div>
+      Glassmorphism Card Hover
+    </div>
+  )
+}
+
+export default Index
