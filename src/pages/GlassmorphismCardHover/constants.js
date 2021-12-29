15ed7c6362233652d5dfc8d7da497b92b65c26c5
jaballogian <jaballogianrma77@gmail.com>
2021-12-29 20:00:15 +0700

    add constant

---
+++ src/pages/GlassmorphismCardHover/constants.js
@@ -0,0 +1,14 @@
+// MATERIAL UI CORES
+import { createTheme, responsiveFontSizes } from '@mui/material/styles'
+
+let customTheme = createTheme({
+  typography: {
+    fontFamily: [
+      'Poppins', 'sans-serif',
+    ].join(','),
+  },
+})
+
+customTheme = responsiveFontSizes(customTheme)
+
+export default customTheme
