474e4af4bbee8dc00099cb98bb8a2989c58af7d1
jaballogian <jaballogianrma77@gmail.com>
2021-12-29 18:58:09 +0700

    add Routes and map pages with Route component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
 
 // PAGES
 import CssWaterDropLogoEffect from 'pages/CssWaterDropLogoEffect/Index'
 
 function App() {
+  const pages = [
+    {
+      id: 1,
+      element: <CssWaterDropLogoEffect/>,
+    },
+  ]
+
   return (
-    <CssWaterDropLogoEffect/>
+    <Routes>
+      {pages.map((item, index) => (
+        <Route 
+          key={index}
+          path={`/clones/${item.id}`} 
+          element={item.element}
+        />
+      ))}
+    </Routes>
   )
 }
 
