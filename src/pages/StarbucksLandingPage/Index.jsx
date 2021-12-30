f65b563878cd91492d29d341e8d1e6122b59e182
jaballogian <jaballogianrma77@gmail.com>
2021-12-30 14:34:35 +0700

    add Theme Provider component

--- src/pages/StarbucksLandingPage/Index.jsx
+++ src/pages/StarbucksLandingPage/Index.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 
+// CONSTANTS
+import customTheme from './constants'
+
 // IMAGES
 import ImageLogo from './assets/logo.png'
 
@@ -11,6 +14,7 @@ import List from '@mui/material/List'
 import ListItem from '@mui/material/ListItem'
 import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography'
+import { ThemeProvider } from '@mui/material/styles'
 
 // STYLES
 import useStyles from './IndexUseStyles'
@@ -26,41 +30,42 @@ const Index = () => {
   ]
 
   return (
-    // ADD POPPINS FONT
-    <Box className={classes.root}>
-      <AppBar position='static'>
-        <Toolbar className={classes.header}>
-          {/* LOGO */}
-          <Link href='#'>
-            <Box
-              component='img'
-              src={ImageLogo}
-              alt=''
-              className={classes.logo}
-            />
-          </Link>
+    <ThemeProvider theme={customTheme}>
+      <Box className={classes.root}>
+        <AppBar position='static'>
+          <Toolbar className={classes.header}>
+            {/* LOGO */}
+            <Link href='#'>
+              <Box
+                component='img'
+                src={ImageLogo}
+                alt=''
+                className={classes.logo}
+              />
+            </Link>
 
-          {/* NAVIGATIONS */}
-          <List className={classes.navigation}>
-            {navigationList.map((item, index) => (
-              <ListItem
-                key={index}
-                className={classes.navigationItem}
-              >
-                <Link 
-                  href='#'
-                  className={classes.navigationLink}
+            {/* NAVIGATIONS */}
+            <List className={classes.navigation}>
+              {navigationList.map((item, index) => (
+                <ListItem
+                  key={index}
+                  className={classes.navigationItem}
                 >
-                  <Typography variant='subtitle1'>
-                    {item}
-                  </Typography>
-                </Link>
-              </ListItem>
-            ))}
-          </List>
-        </Toolbar>
-      </AppBar>
-    </Box>
+                  <Link 
+                    href='#'
+                    className={classes.navigationLink}
+                  >
+                    <Typography variant='subtitle1'>
+                      {item}
+                    </Typography>
+                  </Link>
+                </ListItem>
+              ))}
+            </List>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    </ThemeProvider>
   )
 }
 
