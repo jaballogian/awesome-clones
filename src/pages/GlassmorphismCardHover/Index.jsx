b836d82c3b5e534f5bd0b76b1afe69c05d769760
jaballogian <jaballogianrma77@gmail.com>
2021-12-30 14:32:45 +0700

    delete Styled Engine Provider component

--- src/pages/GlassmorphismCardHover/Index.jsx
+++ src/pages/GlassmorphismCardHover/Index.jsx
@@ -9,9 +9,7 @@ import Card from '@mui/material/Card'
 import Grid from '@mui/material/Grid'
 import Link from '@mui/material/Link'
 import Typography from '@mui/material/Typography'
-
-// MUI STYLES
-import { StyledEngineProvider, ThemeProvider } from '@mui/material/styles'
+import { ThemeProvider } from '@mui/material/styles'
 
 // STYLES
 import useStyles from './IndexUseStyles'
@@ -32,75 +30,73 @@ const Index = () => {
 
   return (
     <ThemeProvider theme={customTheme}>
-      <StyledEngineProvider injectFirst>
-        <Box className={classes.root}>
-          <Grid 
-            container
-            className={classes.container}
-          >
-            {titleList.map((item, index) => (
-              <Tilt
-                key={index}
-                className={classes.tilt}
-                tiltMaxAngleX={35}
-                tiltMaxAngleY={35}
-                transitionSpeed={400}
-                glareEnable={true}
-                glareMaxOpacity={0.5}
-                glareColor='white'
-                glarePosition='all'
+      <Box className={classes.root}>
+        <Grid 
+          container
+          className={classes.container}
+        >
+          {titleList.map((item, index) => (
+            <Tilt
+              key={index}
+              className={classes.tilt}
+              tiltMaxAngleX={35}
+              tiltMaxAngleY={35}
+              transitionSpeed={400}
+              glareEnable={true}
+              glareMaxOpacity={0.5}
+              glareColor='white'
+              glarePosition='all'
+            >
+              <Card 
+                className={classes.card}
+                onMouseEnter={() => setHoveredCard(index)}
+                onMouseLeave={() => setHoveredCard(null)}
               >
-                <Card 
-                  className={classes.card}
-                  onMouseEnter={() => setHoveredCard(index)}
-                  onMouseLeave={() => setHoveredCard(null)}
+                <Box
+                  className={
+                    hoveredCard === index ? 
+                    `${classes.content} ${classes.cardHovered}` : 
+                    classes.content
+                  }
                 >
-                  <Box
-                    className={
-                      hoveredCard === index ? 
-                      `${classes.content} ${classes.cardHovered}` : 
-                      classes.content
-                    }
+                  {/* NUMBER */}
+                  <Typography
+                    variant='h2'
+                    className={classes.number}
                   >
-                    {/* NUMBER */}
-                    <Typography
-                      variant='h2'
-                      className={classes.number}
-                    >
-                      {`0${index + 1}`}
-                    </Typography>
+                    {`0${index + 1}`}
+                  </Typography>
 
-                    {/* TITLE */}
-                    <Typography
-                      variant='h3'
-                      className={classes.title}
-                    >
-                      {item}
-                    </Typography>
+                  {/* TITLE */}
+                  <Typography
+                    variant='h3'
+                    className={classes.title}
+                  >
+                    {item}
+                  </Typography>
 
-                    {/* PARAGRAPH */}
-                    <Typography
-                      variant='subtitle1'
-                      className={classes.paragraph}
-                    >
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                    </Typography>
+                  {/* PARAGRAPH */}
+                  <Typography
+                    variant='subtitle1'
+                    className={classes.paragraph}
+                  >
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  </Typography>
 
-                    {/* LINK */}
-                    <Link 
-                      href='#'
-                      className={classes.link}
-                    >
-                      Read More
-                    </Link>
-                  </Box>
-                </Card>
-              </Tilt>      
-            ))}
-          </Grid>
-        </Box>
-      </StyledEngineProvider>
+                  {/* LINK */}
+                  <Link 
+                    href='#'
+                    className={classes.link}
+                  >
+                    Read More
+                  </Link>
+                </Box>
+              </Card>
+            </Tilt>      
+          ))}
+        </Grid>
+      </Box>
     </ThemeProvider>
   )
 }
