31ba4ce49bc8e65cab4da1ea5982f308a2d714f8
jaballogian <jaballogianrma77@gmail.com>
2021-12-30 17:14:47 +0700

    add new colors

--- src/pages/StarbucksLandingPage/constants.js
+++ src/pages/StarbucksLandingPage/constants.js
@@ -14,6 +14,8 @@ customTheme = responsiveFontSizes(customTheme)
 export default customTheme
 
 export const colors = {
+  deepBlush: '#EB7495', // PINK
   funGreen: '#017143', // GREEN
+  hopbush: '#D752B1', // LIGHT VIOLET
   mineShaft: '#333333', // DARK GRAY
 }
