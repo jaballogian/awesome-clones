9c4d2665809a2e7f81b572a6af517d6918c1549e
jaballogian <jaballogianrma77@gmail.com>
2021-12-31 14:34:23 +0700

    rename styles file

--- src/pages/GlassmorphismCardHover/Index.jsx
+++ src/pages/GlassmorphismCardHover/Index.jsx
@@ -12,7 +12,7 @@ import Typography from '@mui/material/Typography'
 import { ThemeProvider } from '@mui/material/styles'
 
 // STYLES
-import useStyles from './indexUseStyles2'
+import useStyles from './indexUseStyles'
 
 // TILT
 import Tilt from 'react-parallax-tilt'
