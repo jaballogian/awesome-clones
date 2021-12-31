31f73896077e765b00d11f66d39524b89f9d6e4c
jaballogian <jaballogianrma77@gmail.com>
2021-12-31 14:26:19 +0700

    rename styles file

--- src/pages/StarbucksLandingPage/Index.jsx
+++ src/pages/StarbucksLandingPage/Index.jsx
@@ -25,7 +25,7 @@ import IconTwitter from '@mui/icons-material/Twitter'
 import IconInstagram from '@mui/icons-material/Instagram'
 
 // STYLES
-import useStyles from './IndexUseStyles'
+import useStyles from './indexUseStyles'
 
 const Index = () => {
   const classes = useStyles()
