b59bf4427185921bd450ad2e07fd938205513029
jaballogian <jaballogianrma77@gmail.com>
2022-01-03 13:50:59 +0700

    add preview images

--- src/constants/clonedPages.js
+++ src/constants/clonedPages.js
@@ -1,3 +1,11 @@
+// IMAGES
+import Image1 from 'assets/images/screenshots/1.png'
+import Image2 from 'assets/images/screenshots/2.png'
+import Image3 from 'assets/images/screenshots/3.png'
+import Image4 from 'assets/images/screenshots/4.png'
+import Image5 from 'assets/images/screenshots/5.png'
+import Image6 from 'assets/images/screenshots/6.png'
+
 // PAGES
 import WaterDropLogoEffect from 'pages/WaterDropLogoEffect/Index'
 import GlassmorphismCardHover from 'pages/GlassmorphismCardHover/Index'
@@ -11,7 +19,7 @@ const clonedPages = [
     id: 1,
     element: <WaterDropLogoEffect/>,
     source: 'https://www.youtube.com/watch?v=GnllGn9Oifs',
-    preview: '',
+    preview: Image1,
     credits: 'https://github.com/jaballogian/awesome-clones/blob/master/src/pages/WaterDropLogoEffect/credits.md',
     githubCode: 'https://github.com/jaballogian/awesome-clones/tree/master/src/pages/WaterDropLogoEffect',
   },
@@ -19,7 +27,7 @@ const clonedPages = [
     id: 2,
     element: <GlassmorphismCardHover/>,
     source: 'https://www.youtube.com/watch?v=hv0rNxr1XXk',
-    preview: '',
+    preview: Image2,
     credits: 'https://github.com/jaballogian/awesome-clones/blob/master/src/pages/GlassmorphismCardHover/credits.md',
     githubCode: 'https://github.com/jaballogian/awesome-clones/tree/master/src/pages/GlassmorphismCardHover',
   },
@@ -27,7 +35,7 @@ const clonedPages = [
     id: 3,
     element: <StarbucksLandingPage/>,
     source: 'https://www.youtube.com/watch?v=91Q6RvKvd7o',
-    preview: '',
+    preview: Image3,
     credits: 'https://github.com/jaballogian/awesome-clones/blob/master/src/pages/StarbucksLandingPage/credits.md',
     githubCode: 'https://github.com/jaballogian/awesome-clones/tree/master/src/pages/StarbucksLandingPage',
   },
@@ -35,7 +43,7 @@ const clonedPages = [
     id: 4,
     element: <Carousell/>,
     source: 'https://www.youtube.com/watch?v=0wvrlOyGlq0',
-    preview: '',
+    preview: Image4,
     credits: 'https://github.com/jaballogian/awesome-clones/blob/master/src/pages/Carousell/credits.md',
     githubCode: 'https://github.com/jaballogian/awesome-clones/tree/master/src/pages/Carousell',
   },
@@ -43,7 +51,7 @@ const clonedPages = [
     id: 5,
     element: <VideoInsideText/>,
     source: 'https://www.youtube.com/watch?v=S1LyYnyqRRs',
-    preview: '',
+    preview: Image5,
     credits: 'https://github.com/jaballogian/awesome-clones/blob/master/src/pages/VideoInsideText/credits.md',
     githubCode: 'https://github.com/jaballogian/awesome-clones/tree/master/src/pages/VideoInsideText',
   },
@@ -51,7 +59,7 @@ const clonedPages = [
     id: 6,
     element: <CloudBannerParallax/>,
     source: 'https://www.youtube.com/watch?v=4p3YDQLJmSs',
-    preview: '',
+    preview: Image6,
     credits: 'https://github.com/jaballogian/awesome-clones/blob/master/src/pages/CloudBannerParallax/credits.md',
     githubCode: 'https://github.com/jaballogian/awesome-clones/tree/master/src/pages/CloudBannerParallax',
   },
