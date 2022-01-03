59fc48ddbfb88d3686a5aea63f00d7edb2aff090
jaballogian <jaballogianrma77@gmail.com>
2022-01-03 13:42:08 +0700

    add preview, credits, and github code fields

--- src/constants/clonedPages.js
+++ src/constants/clonedPages.js
@@ -6,37 +6,54 @@ import Carousell from 'pages/Carousell/Index'
 import VideoInsideText from 'pages/VideoInsideText/Index'
 import CloudBannerParallax from 'pages/CloudBannerParallax/Index'
 
-// PAGE ITEMS: ID, ELEMENT, SOURCE, CREDITS, GITHUB CODE
 const clonedPages = [
   {
     id: 1,
     element: <WaterDropLogoEffect/>,
     source: 'https://www.youtube.com/watch?v=GnllGn9Oifs',
+    preview: '',
+    credits: 'https://github.com/jaballogian/awesome-clones/blob/master/src/pages/WaterDropLogoEffect/credits.md',
+    githubCode: 'https://github.com/jaballogian/awesome-clones/tree/master/src/pages/WaterDropLogoEffect',
   },
   {
     id: 2,
     element: <GlassmorphismCardHover/>,
     source: 'https://www.youtube.com/watch?v=hv0rNxr1XXk',
+    preview: '',
+    credits: 'https://github.com/jaballogian/awesome-clones/blob/master/src/pages/GlassmorphismCardHover/credits.md',
+    githubCode: 'https://github.com/jaballogian/awesome-clones/tree/master/src/pages/GlassmorphismCardHover',
   },
   {
     id: 3,
     element: <StarbucksLandingPage/>,
     source: 'https://www.youtube.com/watch?v=91Q6RvKvd7o',
+    preview: '',
+    credits: 'https://github.com/jaballogian/awesome-clones/blob/master/src/pages/StarbucksLandingPage/credits.md',
+    githubCode: 'https://github.com/jaballogian/awesome-clones/tree/master/src/pages/StarbucksLandingPage',
   },
   {
     id: 4,
     element: <Carousell/>,
     source: 'https://www.youtube.com/watch?v=0wvrlOyGlq0',
+    preview: '',
+    credits: 'https://github.com/jaballogian/awesome-clones/blob/master/src/pages/Carousell/credits.md',
+    githubCode: 'https://github.com/jaballogian/awesome-clones/tree/master/src/pages/Carousell',
   },
   {
     id: 5,
     element: <VideoInsideText/>,
     source: 'https://www.youtube.com/watch?v=S1LyYnyqRRs',
+    preview: '',
+    credits: 'https://github.com/jaballogian/awesome-clones/blob/master/src/pages/VideoInsideText/credits.md',
+    githubCode: 'https://github.com/jaballogian/awesome-clones/tree/master/src/pages/VideoInsideText',
   },
   {
     id: 6,
     element: <CloudBannerParallax/>,
     source: 'https://www.youtube.com/watch?v=4p3YDQLJmSs',
+    preview: '',
+    credits: 'https://github.com/jaballogian/awesome-clones/blob/master/src/pages/CloudBannerParallax/credits.md',
+    githubCode: 'https://github.com/jaballogian/awesome-clones/tree/master/src/pages/CloudBannerParallax',
   },
 ]
 
