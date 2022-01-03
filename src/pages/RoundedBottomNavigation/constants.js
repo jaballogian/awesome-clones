1bc909c03587320c56ecd3909d49edefd59aafff
jaballogian <jaballogianrma77@gmail.com>
2022-01-03 18:18:52 +0700

    add custom theme

--- src/pages/RoundedBottomNavigation/constants.js
+++ src/pages/RoundedBottomNavigation/constants.js
@@ -1,3 +1,18 @@
+// MUIS
+import { createTheme, responsiveFontSizes } from '@mui/material/styles'
+
+let customTheme = createTheme({
+  typography: {
+    fontFamily: [
+      'Poppins', 'sans-serif',
+    ].join(','),
+  },
+})
+
+customTheme = responsiveFontSizes(customTheme)
+
+export default customTheme
+
 export const colors = {
   shark: '#222327', // DARK GRAY
   springGreen: '#29FD53', // LIGHT GREEN
