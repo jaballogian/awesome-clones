bb9ed88d4844e29f957d93c30240f60d3529838f
jaballogian <jaballogianrma77@gmail.com>
2022-01-03 18:19:02 +0700

    add Theme Provider component

--- src/pages/RoundedBottomNavigation/Index.jsx
+++ src/pages/RoundedBottomNavigation/Index.jsx
@@ -1,10 +1,14 @@
 import React, { useState } from 'react'
 
+// CONSTANTS
+import customTheme from './constants'
+
 // MUIS
 import Box from '@mui/material/Box'
 import List from '@mui/material/List'
 import ListItem from '@mui/material/ListItem'
 import Typography from '@mui/material/Typography'
+import { ThemeProvider } from '@mui/material/styles'
 
 // MUI ICONS
 import IconCameraAltRounded from '@mui/icons-material/CameraAltRounded'
@@ -45,45 +49,47 @@ const Index = () => {
   ]
 
   return (
-    <Box className={classes.root}>
-      <List className={classes.navigationsList}>
-        {/* SELECTED ITEM */}
-        <Box 
-          className={classes.selectedItem}
-          sx={{ transform: `translateX(calc(70 * ${activeItem}px))` }}
-        />
+    <ThemeProvider theme={customTheme}>
+      <Box className={classes.root}>
+        <List className={classes.navigationsList}>
+          {/* SELECTED ITEM */}
+          <Box 
+            className={classes.selectedItem}
+            sx={{ transform: `translateX(calc(70 * ${activeItem}px))` }}
+          />
 
-        {/* NAVIGATIONS LIST */}
-        {navigationsList.map((item, index) => (
-          <ListItem
-            key={index}
-            className={classes.navigationItem}
-            onClick={() => setActiveItem(index)}
-          >
-            {/* IMAGE */}
-            <item.icon 
-              className={
-                activeItem === index ?
-                `${classes.navigationIcon} ${classes.navigationIconActive}` :
-                classes.navigationIcon
-              }
-            />
-
-            {/* TEXT */}
-            <Typography 
-              variant='subtitle1'
-              className={
-                activeItem === index ?
-                `${classes.navigationText} ${classes.navigationTextActive}` :
-                classes.navigationText
-              }
+          {/* NAVIGATIONS LIST */}
+          {navigationsList.map((item, index) => (
+            <ListItem
+              key={index}
+              className={classes.navigationItem}
+              onClick={() => setActiveItem(index)}
             >
-              {item.text}
-            </Typography>
-          </ListItem>
-        ))}
-      </List>
-    </Box>
+              {/* IMAGE */}
+              <item.icon 
+                className={
+                  activeItem === index ?
+                  `${classes.navigationIcon} ${classes.navigationIconActive}` :
+                  classes.navigationIcon
+                }
+              />
+
+              {/* TEXT */}
+              <Typography 
+                variant='subtitle1'
+                className={
+                  activeItem === index ?
+                  `${classes.navigationText} ${classes.navigationTextActive}` :
+                  classes.navigationText
+                }
+              >
+                {item.text}
+              </Typography>
+            </ListItem>
+          ))}
+        </List>
+      </Box>
+    </ThemeProvider>
   )
 }
 
