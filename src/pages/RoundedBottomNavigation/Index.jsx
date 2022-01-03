286aa588a9dc983433b809c2ac1d3670c409b746
jaballogian <jaballogianrma77@gmail.com>
2022-01-03 17:28:20 +0700

    add some components

--- src/pages/RoundedBottomNavigation/Index.jsx
+++ src/pages/RoundedBottomNavigation/Index.jsx
@@ -1,10 +1,69 @@
 import React from 'react'
 
+// MUIS
+import Box from '@mui/material/Box'
+import List from '@mui/material/List'
+import ListItem from '@mui/material/ListItem'
+import Typography from '@mui/material/Typography'
+
+// MUI ICONS
+import IconCameraAltRounded from '@mui/icons-material/CameraAltRounded'
+import IconChatRounded from '@mui/icons-material/ChatRounded'
+import IconHomeRounded from '@mui/icons-material/HomeRounded'
+import IconPersonRounded from '@mui/icons-material/PersonRounded'
+import IconSettingsRounded from '@mui/icons-material/SettingsRounded'
+
+// STYLES
+import useStyles from './indexUseStyles'
+
 const Index = () => {
+  const classes = useStyles()
+
+  const navigationsList = [
+    {
+      icon: IconHomeRounded,
+      text: 'Home',
+    },
+    {
+      icon: IconPersonRounded,
+      text: 'Profile',
+    },
+    {
+      icon: IconChatRounded,
+      text: 'Message',
+    },
+    {
+      icon: IconCameraAltRounded,
+      text: 'Photo',
+    },
+    {
+      icon: IconSettingsRounded,
+      text: 'Setting',
+    },
+  ]
+
   return (
-    <div>
-      Rounded Bottom Navigation
-    </div>
+    <Box className={classes.root}>
+      <List className={classes.navigationsList}>
+        {navigationsList.map((item, index) => (
+          <ListItem
+            key={index}
+            className={classes.navigationItem}
+          >
+            {/* IMAGE */}
+            <item.icon className={classes.navigationIcon}/>
+
+            {/* TEXT */}
+            <Typography 
+              variant='subtitle1'
+              className={classes.navigationText}
+            >
+              {item.text}
+            </Typography>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
   )
 }
 
