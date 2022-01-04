6386f8f02d89ca0a3cecf72e6edce520dd25986a
jaballogian <jaballogianrma77@gmail.com>
2022-01-04 16:45:39 +0700

    map navigations list with List Item components

--- src/pages/RoundedSidebarMenu/Index.jsx
+++ src/pages/RoundedSidebarMenu/Index.jsx
@@ -1,10 +1,79 @@
 import React from 'react'
 
+// MUIS
+import Box from '@mui/material/Box'
+import List from '@mui/material/List'
+import ListItem from '@mui/material/ListItem'
+import Typography from '@mui/material/Typography'
+
+// MUI ICONS
+import IconAccountCircleRounded from '@mui/icons-material/AccountCircleRounded'
+import IconChatRounded from '@mui/icons-material/ChatRounded'
+import IconHelpRounded from '@mui/icons-material/HelpRounded'
+import IconHomeRounded from '@mui/icons-material/HomeRounded'
+import IconLockRounded from '@mui/icons-material/LockRounded'
+import IconLogoutRounded from '@mui/icons-material/LogoutRounded'
+import IconSettings from '@mui/icons-material/Settings'
+
+// STYLES
+import useStyles from './indexUseStyles'
+
 const Index = () => {
+  const classes = useStyles()
+
+  const navigationsList = [
+    {
+      icon: IconHomeRounded,
+      text: 'Home',
+    },
+    {
+      icon: IconAccountCircleRounded,
+      text: 'Home',
+    },
+    {
+      icon: IconChatRounded,
+      text: 'Messages',
+    },
+    {
+      icon: IconSettings,
+      text: 'Setting',
+    },
+    {
+      icon: IconHelpRounded,
+      text: 'Help',
+    },
+    {
+      icon: IconLockRounded,
+      text: 'Password',
+    },
+    {
+      icon: IconLogoutRounded,
+      text: 'Sign Out',
+    },
+  ]
+
   return (
-    <div>
-      Rounded Sidebar Menu
-    </div>
+    <Box className={classes.root}>
+      <List className={classes.navigationContainer}>
+        {navigationsList.map((item, index) => (
+          <ListItem
+            key={index}
+            className={classes.navigationItem}
+          >
+            {/* ICON */}
+            <item.icon className={classes.navigationIcon}/>
+
+            {/* TEXT */}
+            <Typography
+              variant='subtitle1'
+              className={classes.navigationText}
+            >
+              {item.text}
+            </Typography>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
   )
 }
 
