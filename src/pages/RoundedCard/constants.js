794464fbf9695c57b32a98ab9b96626a9db104f2
jaballogian <jaballogianrma77@gmail.com>
2022-01-06 21:14:36 +0700

    add colors and box shadows

---
+++ src/pages/RoundedCard/constants.js
@@ -0,0 +1,9 @@
+export const colors = {
+  dodgerBlue: '#287BFF', // BLUE
+  martinique: '#3C2846', // DARK VIOLET
+}
+
+export const boxShadows = {
+  softWhite: 'rgba(255, 255, 255, 0.25)',
+  softBlack: 'rgba(0, 0, 0, 0.15)',
+}
