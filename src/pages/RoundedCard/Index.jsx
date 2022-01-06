64d26c166006a480a5383e82ea4badf75422f919
jaballogian <jaballogianrma77@gmail.com>
2022-01-06 21:14:24 +0700

    add some components

--- src/pages/RoundedCard/Index.jsx
+++ src/pages/RoundedCard/Index.jsx
@@ -1,10 +1,47 @@
 import React from 'react'
 
+// MUIS
+import Box from '@mui/material/Box'
+import Card from '@mui/material/Card'
+import Typography from '@mui/material/Typography'
+
+// STYLES
+import useStyles from './indexUseStyles'
+
 const Index = () => {
+  const classes = useStyles()
+
+  const cardsList = [
+    'Card One',
+    'Card Two',
+    'Card Three',
+  ]
+
   return (
-    <div>
-      Rounded Card
-    </div>
+    <Box className={classes.root}>
+      {cardsList.map((item, index) => (
+        <Card
+          key={index}
+          className={classes.card}
+        >
+          {/* TITLE */}
+          <Typography
+            variant='h2'
+            className={classes.cardTitle}
+          >
+            {item}
+          </Typography>
+
+          {/* CAPTION */}
+          <Typography
+            variant='subtitle1'
+            className={classes.cardCaption}
+          >
+            {item}
+          </Typography>
+        </Card>
+      ))}
+    </Box>
   )
 }
 
