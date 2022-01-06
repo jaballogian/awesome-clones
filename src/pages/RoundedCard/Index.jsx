fbfae6ab5e8bf3f7e202f34695ffedb77fefa5ff
jaballogian <jaballogianrma77@gmail.com>
2022-01-06 21:44:49 +0700

    add icons

--- src/pages/RoundedCard/Index.jsx
+++ src/pages/RoundedCard/Index.jsx
@@ -5,6 +5,11 @@ import Box from '@mui/material/Box'
 import Card from '@mui/material/Card'
 import Typography from '@mui/material/Typography'
 
+// MUI ICONS
+import IconDiamondOutlined from '@mui/icons-material/DiamondOutlined'
+import IconPublicOutlined from '@mui/icons-material/PublicOutlined'
+import IconRocketLaunchOutlined from '@mui/icons-material/RocketLaunchOutlined'
+
 // STYLES
 import useStyles from './indexUseStyles'
 
@@ -12,9 +17,18 @@ const Index = () => {
   const classes = useStyles()
 
   const cardsList = [
-    'Card One',
-    'Card Two',
-    'Card Three',
+    {
+      icon: IconDiamondOutlined,
+      title: 'Card One',
+    },
+    {
+      icon: IconPublicOutlined,
+      title: 'Card Two',
+    },
+    {
+      icon: IconRocketLaunchOutlined,
+      title: 'Card Three',
+    },
   ]
 
   return (
@@ -24,21 +38,26 @@ const Index = () => {
           key={index}
           className={classes.card}
         >
+          {/* ICON */}
+          <Box className={classes.cardIconContainer}>
+            <item.icon className={classes.cardIcon}/>
+          </Box>
+          
           {/* TITLE */}
-          <Typography
+          {/* <Typography
             variant='h2'
             className={classes.cardTitle}
           >
-            {item}
-          </Typography>
+            {item.title}
+          </Typography> */}
 
           {/* CAPTION */}
-          <Typography
+          {/* <Typography
             variant='subtitle1'
             className={classes.cardCaption}
           >
-            {item}
-          </Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          </Typography> */}
         </Card>
       ))}
     </Box>
