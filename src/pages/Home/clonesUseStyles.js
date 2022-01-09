87a919c2fefe29a7cfd149d24cf2b6d5105a0b60
jaballogian <jaballogianrma77@gmail.com>
2022-01-09 17:59:20 +0700

    add Clones component and its styles

---
+++ src/pages/Home/clonesUseStyles.js
@@ -0,0 +1,31 @@
+// MUI STYLES
+import { makeStyles } from '@mui/styles'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+    padding: 40,
+  },
+  cardRoot: {
+    position: 'relative',
+    transform: 'scale(1)',
+    transition: '0.25s ease-in-out',
+    '&:hover': {
+      transform: 'scale(1.05)',
+    },
+  },
+  cardImage: {
+    width: '100%',
+    objectFit: 'cover',
+  },
+  cardTitle: {
+    textAlign: 'center',
+  },
+  cardsIconContainer: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+}))
+
+export default useStyles
